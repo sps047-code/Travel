06c89f8edc87fa883df95059825aa6a5541aa468
sps047-code <sps047@gmail.com>
2026-06-01 10:05:34 -0400

    test

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
 //      e.g. trip.html (HTML+CSS) + trip.js (JavaScript) — already done.
 // =============================================================================
 
-const CACHE = 'seasons-v33';
+const CACHE = 'seasons-v34';
 const PRECACHE = [
   '/Travel/index.html',
   '/Travel/trip.html',
